7f34155df4287be3b40263e894d6ec92e8da1b1d
PeteHaberlNC <peter.haberl@nordcloud.com>
2024-10-24 17:07:14 +0200

    iterations and url as parameters

--- do_requests.py
+++ do_requests.py
@@ -1,10 +1,20 @@
 import requests
 import socket
 import datetime
+import argparse
+
+# Erstelle einen Argumentparser
+parser = argparse.ArgumentParser(description='Anzahl messages und URL')
+parser.add_argument('number', type=int, help='Iterations')
+parser.add_argument('text', type=str, help='Payload')
+
+# Lese die Argumente von der Kommandozeile
+args = parser.parse_args()
+url = args.text
 
 #http://182.92.141.163:8000//api/analytics
 #url = "https://flow-poc.apps.ocp420241004074046.openshiftlabs.online/api/analytics"
-url = "http://182.92.141.163:8000//api/analytics"
+#url = "http://182.92.141.163:8000/api/analytics"
 
 timestamp = datetime.datetime.now().isoformat()
 local_ip = socket.gethostbyname(socket.gethostname())
@@ -13,7 +23,7 @@ data = {
     "local IP": local_ip
 }
 
-for i in range(10):
+for i in range(args.number):
     response = requests.post(url, json=data, verify=False)
     # Print the response
     print(response.json())
